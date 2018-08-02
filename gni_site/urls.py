68bb484bd42ab2fe37f55684b729f5882f2da35e
Pastor Emmanuel <pastorenuel@gmail.com>
2018-08-02 08:47:52 +0200

    updated urls.py

--- gni_site/urls.py
+++ gni_site/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import STATIC_ROOT
+from django.conf import settings
 from django.views.generic.base import TemplateView
 
 
